e5d28e20028cbf7d837ad2c8c6a3654382b5a39c
W3L17 <bleyding7103@gmail.com>
2025-10-12 14:38:58 +0300

    Урок 3

--- lesson_1_task_3.py
+++
@@ -1,4 +0,0 @@
-my_name = "Valera"
-my_lastname = "Khalzev"
-
-input ("Вас зовут:" + my_name + my_lastname)
--- lesson_1_task_4.py
+++
@@ -1,5 +0,0 @@
-def print_greeting():
-    greeting = "Привет мир!"
-    print(greeting)
-
-print_greeting()
